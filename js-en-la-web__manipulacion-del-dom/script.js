cec0a420de2277216c461b62146dadbd038f71fc
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-07-19 17:04:55 -0600

    :heavy_plus_sign: ADD: Creando un elemento

--- js-en-la-web__manipulacion-del-dom/script.js
+++ js-en-la-web__manipulacion-del-dom/script.js
@@ -1,9 +1,22 @@
-const botonSubmit = document.querySelector('[data-form-btn]');
-
 const createTask = (evento) => {
     evento.preventDefault();
-    const taskInput = document.querySelector('[data-form-input]');
-    console.log(taskInput.value);
+    const formTaskInput = document.querySelector('[data-form-input]');
+    const taskTitle = formTaskInput.value;
+	const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+	task.classList.add('card');
+    formTaskInput.value = "";
+
+    //backticks
+    const taskContent = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${taskTitle}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = taskContent;
+    list.appendChild(task);
 }
+
+const botonSubmit = document.querySelector('[data-form-btn]');
 
 botonSubmit.addEventListener('click', createTask);
