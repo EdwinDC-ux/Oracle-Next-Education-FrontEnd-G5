bed1f30263ad85aa68f60331a0bc0b13cd69133e
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-07-19 23:07:20 -0600

    :heavy_plus_sign: ADD: Creando el botón concluir

--- js-en-la-web__manipulacion-del-dom/script.js
+++ js-en-la-web__manipulacion-del-dom/script.js
@@ -8,15 +8,35 @@ const createTask = (evento) => {
     formTaskInput.value = "";
 
     //backticks
-    const taskContent = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${taskTitle}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = taskContent;
+    const taskContent = document.createElement('div');
+    taskContent.appendChild(checkComplete());
+
+	const elementTaskTitle = document.createElement('span');
+	elementTaskTitle.classList.add('task');
+	elementTaskTitle.innerText = taskTitle;
+	taskContent.appendChild(elementTaskTitle);
+	
+	task.appendChild(taskContent);
+
+	const trashIcon = document.createElement('i');
+	trashIcon.classList.add('fas');
+	trashIcon.classList.add('fa-trash-alt');
+	trashIcon.classList.add('trashIcon');
+	trashIcon.classList.add('icon');
+	task.appendChild(trashIcon);
+
     list.appendChild(task);
 }
 
 const botonSubmit = document.querySelector('[data-form-btn]');
 
 botonSubmit.addEventListener('click', createTask);
+
+const checkComplete = () => {
+	const checkIcon = document.createElement('i');
+	checkIcon.classList.add('far');
+	checkIcon.classList.add('fa-check-square');
+	checkIcon.classList.add('icon');
+
+	return checkIcon
+}
