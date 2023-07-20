b80023cd154afd73cad4d787df6c4567c2bb3538
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-07-20 07:28:58 -0600

    :heavy_plus_sign: ADD: JavaScript IIFE.
    Immediately Invoked Function Expression (IIFE)

--- js-en-la-web__manipulacion-del-dom/script.js
+++ js-en-la-web__manipulacion-del-dom/script.js
@@ -1,44 +1,46 @@
-const createTask = (evento) => {
-    evento.preventDefault();
-    const formTaskInput = document.querySelector('[data-form-input]');
-    const taskTitle = formTaskInput.value;
-	const list = document.querySelector('[data-list]');
-    const task = document.createElement('li');
-	task.classList.add('card');
-    formTaskInput.value = "";
+( () => {
+	const createTask = (evento) => {
+		evento.preventDefault();
+		const formTaskInput = document.querySelector('[data-form-input]');
+		const taskTitle = formTaskInput.value;
+		const list = document.querySelector('[data-list]');
+		const task = document.createElement('li');
+		task.classList.add('card');
+		formTaskInput.value = "";
 
-    const taskContent = document.createElement('div');
-    taskContent.appendChild(checkComplete());
+		const taskContent = document.createElement('div');
+		taskContent.appendChild(checkComplete());
 
-	const elementTaskTitle = document.createElement('span');
-	elementTaskTitle.classList.add('task');
-	elementTaskTitle.innerText = taskTitle;
-	taskContent.appendChild(elementTaskTitle);
-	
-	task.appendChild(taskContent);
+		const elementTaskTitle = document.createElement('span');
+		elementTaskTitle.classList.add('task');
+		elementTaskTitle.innerText = taskTitle;
+		taskContent.appendChild(elementTaskTitle);
+		
+		task.appendChild(taskContent);
 
-	const trashIcon = document.createElement('i');
-	trashIcon.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
-	task.appendChild(trashIcon);
+		const trashIcon = document.createElement('i');
+		trashIcon.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
+		task.appendChild(trashIcon);
 
-    list.appendChild(task);
-};
+		list.appendChild(task);
+	};
 
-const botonSubmit = document.querySelector('[data-form-btn]');
+	const botonSubmit = document.querySelector('[data-form-btn]');
 
-botonSubmit.addEventListener('click', createTask);
+	botonSubmit.addEventListener('click', createTask);
 
-const checkComplete = () => {
-	const checkIcon = document.createElement('i');
-	checkIcon.classList.add('far', 'fa-check-square', 'icon');
-	checkIcon.addEventListener('click', completeTask);
+	const checkComplete = () => {
+		const checkIcon = document.createElement('i');
+		checkIcon.classList.add('far', 'fa-check-square', 'icon');
+		checkIcon.addEventListener('click', completeTask);
 
-	return checkIcon;
-};
+		return checkIcon;
+	};
 
-const completeTask = (event) => {
-	const element = event.target;
-	element.classList.toggle('fas');
-	element.classList.toggle('completeIcon');
-	element.classList.toggle('far');
-};
+	const completeTask = (event) => {
+		const element = event.target;
+		element.classList.toggle('fas');
+		element.classList.toggle('completeIcon');
+		element.classList.toggle('far');
+	};
+})();
