d572d80df607bcb48eaf2399a7077befc68ea3f0
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-02 10:50:32 -0600

    :heavy_plus_sign: ADD: Tipos de errores

--- js-en-la-web__validacion-de-formularios/js/validaciones.js
+++ js-en-la-web__validacion-de-formularios/js/validaciones.js
@@ -1,43 +1,67 @@
 const validadores = {
-    nacimiento: (input) => validarNacimiento(input)
+    'nacimiento': (input) => validarNacimiento(input)
 };
+
+const tipoErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError"
+];
 
 const mensajesError = {
     nombre: {
-        valueMissing: "Este campo no puede estar vacío"
+        valueMissing: "El campo nombre no puede estar vacío"
     },
     email: {
-        valueMissing: "Este campo no puede estar vacío",
+        valueMissing: "El campo correo no puede estar vacío",
         typeMismatch: "El correo no es válido"
     },
     password: {
-        valueMissing: "Este campo no puede estar vacío",
+        valueMissing: "El campo contraseña no puede estar vacío",
         patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales."
     },
     nacimiento: {
-        valueMissing: "Este campo no puede estar vacío",
+        valueMissing: "El campo fecha de nacimiento no puede estar vacío",
         customError: "Debes tener al menos 18 años de edad"
     }
-}
+};
 
 export function valida(input) {
     const tipoInput = input.dataset.tipo;
+    console.log(tipoInput, validadores[tipoInput])
     if (validadores[tipoInput]) {
         validadores[tipoInput](input);
     }
 
     if (input.validity.valid) {
         input.parentElement.classList.remove('input-container--invalid');
+        input.parentElement.querySelector('.input-message-error').innerHTML = "";
     } else {
         input.parentElement.classList.add('input-container--invalid');
+        input.parentElement.querySelector('.input-message-error').innerHTML = mostrarMensajeError(tipoInput, input);
     }
+}
+
+function mostrarMensajeError(tipoInput, input) {
+    let mensaje = "";
+    tipoErrores.forEach(error => {
+        if (input.validity[error]) {
+            console.log(tipoInput, error);
+            console.log(input.validity[error]);
+            console.log(mensajesError[tipoInput][error]);
+            mensaje = mensajesError[tipoInput][error];
+        }
+    });
+
+    return mensaje;
 }
 
 function validarNacimiento(input) {
     const fecha = new Date(input.value);
     let mensaje = '';
 
-    if(!mayorDeEdad(fecha)) {
+    if(!mayorDeEdad(fecha) && input.value != "") {
         mensaje = 'Debes tener al menos 18 años de edad';
     }
 
