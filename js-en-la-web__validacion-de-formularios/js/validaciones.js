bc488c1071332a4ad9a81ee72ed4080354ae6939
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-02 09:08:58 -0600

    :heavy_plus_sign: ADD: Mostrando mensajes de error

--- js-en-la-web__validacion-de-formularios/js/validaciones.js
+++ js-en-la-web__validacion-de-formularios/js/validaciones.js
@@ -2,10 +2,34 @@ const validadores = {
     nacimiento: (input) => validarNacimiento(input)
 };
 
+const mensajesError = {
+    nombre: {
+        valueMissing: "Este campo no puede estar vacío"
+    },
+    email: {
+        valueMissing: "Este campo no puede estar vacío",
+        typeMismatch: "El correo no es válido"
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacío",
+        patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales."
+    },
+    nacimiento: {
+        valueMissing: "Este campo no puede estar vacío",
+        customError: "Debes tener al menos 18 años de edad"
+    }
+}
+
 export function valida(input) {
     const tipoInput = input.dataset.tipo;
     if (validadores[tipoInput]) {
         validadores[tipoInput](input);
+    }
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalid');
+    } else {
+        input.parentElement.classList.add('input-container--invalid');
     }
 }
 
