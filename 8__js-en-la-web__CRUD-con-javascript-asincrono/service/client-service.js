86a1a7ec54e1917e0fb5f39c6185410cdfb1207a
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-10 08:53:44 -0600

    :heavy_plus_sign: ADD: Creando templates.

--- 8__js-en-la-web__CRUD-con-javascript-asincrono/service/client-service.js
+++ 8__js-en-la-web__CRUD-con-javascript-asincrono/service/client-service.js
@@ -1,3 +1,35 @@
+const crearNuevaLinea = (nombre, email) => {
+    const linea = document.createElement('tr')
+    const contenido = `
+    <td class="td" data-td>${nombre}</td>
+    <td>${email}</td>
+    <td>
+    <ul class="table__button-control">
+        <li>
+        <a
+            href="../screens/editar_cliente.html"
+            class="simple-button simple-button--edit"
+            >Editar</a
+        >
+        </li>
+        <li>
+        <button
+            class="simple-button simple-button--delete"
+            type="button"
+        >
+            Eliminar
+        </button>
+        </li>
+    </ul>
+    </td>`;
+
+    linea.innerHTML = contenido;
+
+    return linea;
+}
+
+const table = document.querySelector('[data-table]');
+
 const http = new XMLHttpRequest();
 
 //Abrir http (metodo, url)
@@ -14,6 +46,15 @@ http.open("GET", 'http://localhost:3000/perfil');
 http.send();
 
 http.onload = () => {
-    const data = http.response;
-    console.log(data);
+    const data = JSON.parse(http.response);
+    data.forEach(perfil => {
+        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+        table.appendChild(nuevaLinea);
+    });
+    const http2 = new XMLHttpRequest();
+    http.open("GET", 'http://localhost:3000/perfil/hoy');
+    http.send();
+    http.onload = () => {
+        const data2 = JSON.parse(http2.response);
+    };
 };
