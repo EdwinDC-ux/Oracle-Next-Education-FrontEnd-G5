b46d04718a8ad309e9ba2e59f06688e75fa035fd
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-18 09:26:29 -0600

    :heavy_plus_sign: ADD: Componente CampoTexto

---
+++ 9__react__desarrollando-con-JavaScript/org/src/componentes/CampoTexto/index.js
@@ -0,0 +1,10 @@
+import './CampoTexto.css';
+
+function CampoTexto() {
+    return <div className="campo-texto">
+        <label>Nombre</label>
+        <input placeholder="Ingresar nombre" />
+    </div>
+}
+
+export default CampoTexto;
