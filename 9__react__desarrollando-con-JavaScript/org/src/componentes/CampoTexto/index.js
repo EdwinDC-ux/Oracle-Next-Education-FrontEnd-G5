fa636974cbbba0d25c12f1bf51e9f4756d671503
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-18 09:42:05 -0600

    :heavy_plus_sign: ADD: Props

--- 9__react__desarrollando-con-JavaScript/org/src/componentes/CampoTexto/index.js
+++ 9__react__desarrollando-con-JavaScript/org/src/componentes/CampoTexto/index.js
@@ -1,9 +1,10 @@
 import './CampoTexto.css';
 
-function CampoTexto() {
+function CampoTexto(props) {
+    const placeholderModificado = `${props.placeholder}...`
     return <div className="campo-texto">
-        <label>Nombre</label>
-        <input placeholder="Ingresar nombre" />
+        <label>{ props.titulo }</label>
+        <input placeholder={ placeholderModificado } />
     </div>
 }
 
