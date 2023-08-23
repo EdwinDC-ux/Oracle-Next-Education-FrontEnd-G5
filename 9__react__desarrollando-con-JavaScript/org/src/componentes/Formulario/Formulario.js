a19b86d69adab00f41b61d329f103eb050f3f5f4
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-23 07:15:29 -0600

    :heavy_plus_sign: ADD: Campos requeridos

--- 9__react__desarrollando-con-JavaScript/org/src/componentes/Formulario/Formulario.js
+++ 9__react__desarrollando-con-JavaScript/org/src/componentes/Formulario/Formulario.js
@@ -7,15 +7,15 @@ const Formulario = () => {
 
     const manejarEnvio = (evento) => {
         evento.preventDefault();
-        console.log('Manejar el envio');
+        console.log('Manejar el envio', evento);
     }
 
     return <section className="formulario">
         <form onSubmit={manejarEnvio}>
             <h2>Rellena el formuilario para crear el colaborador.</h2>
-            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" />
-            <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" />
-            <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" />
+            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required = {true} />
+            <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required />
+            <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" required />
             <ListaOpciones />
             <Boton>Crear</Boton>
         </form>
