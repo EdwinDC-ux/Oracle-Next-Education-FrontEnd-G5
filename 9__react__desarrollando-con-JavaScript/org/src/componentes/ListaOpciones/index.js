1f2cef64cc7cc771d21da87653f5d0330aba2343
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-23 10:36:29 -0600

    :heavy_plus_sign: ADD: Refactorizando el código

--- 9__react__desarrollando-con-JavaScript/org/src/componentes/ListaOpciones/index.js
+++ 9__react__desarrollando-con-JavaScript/org/src/componentes/ListaOpciones/index.js
@@ -1,15 +1,6 @@
 import './ListaOpciones.css'
 
 function ListaOpciones(props) {
-    const equipos = [
-        "Programación",
-        "Front End",
-        "Data Science",
-        "Devops",
-        "UX y Diseño",
-        "Móvil",
-        "Innovación y Gestión"
-    ];
 
     const manejarCambio = (e) => {
         props.actualizarEquipo(e.target.value);
@@ -19,7 +10,7 @@ function ListaOpciones(props) {
         <label>Equipos</label>
         <select value={props.valor} onChange={manejarCambio}>
             <option value="" disabled defaultValue="" hidden>Seleccionar equipo</option>
-            { equipos.map( ( equipo, index ) => <option key={index} value={equipo}>{equipo}</option> ) }
+            { props.equipos.map( ( equipo, index ) => <option key={index} value={equipo}>{equipo}</option> ) }
         </select>
     </div>
 }
