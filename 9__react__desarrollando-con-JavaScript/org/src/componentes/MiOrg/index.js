b07ee1960dcbf7f813ae85b0e0039252c6864adf
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-23 07:55:03 -0600

    :heavy_plus_sign: ADD: Estado con React

--- 9__react__desarrollando-con-JavaScript/org/src/componentes/MiOrg/index.js
+++ 9__react__desarrollando-con-JavaScript/org/src/componentes/MiOrg/index.js
@@ -1,9 +1,23 @@
+import { useState } from "react"
 import './MiOrg.css';
 
 function MiOrg() {
+    // Estado - hooks
+    // useState
+    //useState()
+    //const [nombreVariable,functionActualiza] = useState(valorInicial)
+
+    const [nombre, actualizarNombre] = useState("harland");
+    const [mostrar, actualizarMostrar] = useState(true);
+
+    const manejarClick = () => {
+        console.log("Mostrar/Ocultar elemento");
+        actualizarMostrar(!mostrar);
+    }
+
     return <section className="orgSection">
-        <h3 className ="title">Mi Organización</h3>
-        <img src="/img/add.png" alt="add" />
+        <h3 className ="title">Mi Organización {nombre} </h3>
+        <img src="/img/add.png" alt="add" onClick = { manejarClick } />
     </section>
 }
 
