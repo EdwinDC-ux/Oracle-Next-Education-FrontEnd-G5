243775fc7440eafe5640da376fe5dd050f2cb8c3
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-24 10:55:07 -0600

    :heavy_plus_sign: ADD: Creando Equipo

--- 9__react__desarrollando-con-JavaScript/org/src/componentes/Formulario/Formulario.js
+++ 9__react__desarrollando-con-JavaScript/org/src/componentes/Formulario/Formulario.js
@@ -11,7 +11,11 @@ const Formulario = (props) => {
     const [foto,actualizarFoto] = useState('');
     const [equipo,actualizarEquipo] = useState('');
 
-    const { registrarColaborador } = props;
+    
+    const [titulo,actualizarTitulo] = useState('');
+    const [color,actualizarColor] = useState('');
+
+    const { registrarColaborador, crearEquipo } = props;
 
     const manejarEnvio = (evento) => {
         evento.preventDefault();
@@ -23,6 +27,11 @@ const Formulario = (props) => {
         };
 
         registrarColaborador(datosAEnviar);
+    }
+
+    const manejarNuevoEquipo = (e) => {
+        e.preventDefault();
+        crearEquipo({ titulo, colorPrimario: color });
     }
 
     return <section className="formulario">
@@ -55,6 +64,24 @@ const Formulario = (props) => {
                 equipos={props.equipos}
             />
             <Boton>Crear</Boton>
+        </form>
+        <form onSubmit={manejarNuevoEquipo}>
+            <h2>Rellena el formuilario para crear el equipo.</h2>
+            <CampoTexto
+                titulo="Titulo"
+                placeholder="Ingresar titulo"
+                required = {true}
+                valor={titulo}
+                actualizarValor={actualizarTitulo}
+            />
+            <CampoTexto
+                titulo="Color"
+                placeholder="Ingresar el color en hex"
+                required
+                valor={color}
+                actualizarValor={actualizarColor}
+            />
+            <Boton>Registrar equipo</Boton>
         </form>
     </section>
 }
