ef149f7750e7003796fed8e90242399ccc9137f2
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-24 10:33:08 -0600

    :heavy_plus_sign: ADD: Eliminando colaboradores

--- 9__react__desarrollando-con-JavaScript/org/src/App.js
+++ 9__react__desarrollando-con-JavaScript/org/src/App.js
@@ -104,8 +104,10 @@ function App() {
   }
 
   //Eliminar colaborador
-  const eliminarColaborador = () => {
-    console.log('Eliminar colaborador');
+  const eliminarColaborador = (id) => {
+    console.log('Eliminar colaborador', id);
+    const nuevosColaboradores = colaboradores.filter((colaborador) => colaborador.id != id)
+    actualizarColaboradores(nuevosColaboradores)
   }
 
   //Actualizar color de equipos
