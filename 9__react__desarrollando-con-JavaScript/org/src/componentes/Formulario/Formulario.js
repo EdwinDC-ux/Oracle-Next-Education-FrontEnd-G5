b4d769eee139fcb08ba76da07e8c73adb8e3135b
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-24 12:50:16 -0600

    :heavy_plus_sign: ADD: Refactorizando formulario

--- 9__react__desarrollando-con-JavaScript/org/src/componentes/Formulario/Formulario.js
+++ 9__react__desarrollando-con-JavaScript/org/src/componentes/Formulario/Formulario.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Formulario.css';
-import CampoTexto from '../CampoTexto';
+import Campo from '../Campo';
 import ListaOpciones from '../ListaOpciones';
 import Boton from '../Boton';
 
@@ -37,21 +37,21 @@ const Formulario = (props) => {
     return <section className="formulario">
         <form onSubmit={manejarEnvio}>
             <h2>Rellena el formuilario para crear el colaborador.</h2>
-            <CampoTexto
+            <Campo
                 titulo="Nombre"
                 placeholder="Ingresar nombre"
                 required = {true}
                 valor={nombre}
                 actualizarValor={actualizarNombre}
             />
-            <CampoTexto
+            <Campo
                 titulo="Puesto"
                 placeholder="Ingresar puesto"
                 required
                 valor={puesto}
                 actualizarValor={actualizarPuesto}
             />
-            <CampoTexto
+            <Campo
                 titulo="Foto"
                 placeholder="Ingresar enlace de foto"
                 required
@@ -67,19 +67,20 @@ const Formulario = (props) => {
         </form>
         <form onSubmit={manejarNuevoEquipo}>
             <h2>Rellena el formuilario para crear el equipo.</h2>
-            <CampoTexto
+            <Campo
                 titulo="Titulo"
                 placeholder="Ingresar titulo"
                 required = {true}
                 valor={titulo}
                 actualizarValor={actualizarTitulo}
             />
-            <CampoTexto
+            <Campo
                 titulo="Color"
                 placeholder="Ingresar el color en hex"
                 required
                 valor={color}
                 actualizarValor={actualizarColor}
+                type='color'
             />
             <Boton>Registrar equipo</Boton>
         </form>
