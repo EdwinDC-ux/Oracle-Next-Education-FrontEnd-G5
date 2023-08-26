7a5b350daf45c85705e78fe1e45da9b72c71237a
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-26 13:24:36 -0600

    :heavy_plus_sign: ADD: Conexión con API

---
+++ 11__react-router__navegacion-en-una-spa/src/components/listPosts.jsx
@@ -0,0 +1,12 @@
+import '../assets/css/componentes/card.css';
+
+function ListPosts({ url }) {
+
+    return (
+        <section className='posts container'>
+
+        </section>
+    );
+}
+
+export default ListPosts;
