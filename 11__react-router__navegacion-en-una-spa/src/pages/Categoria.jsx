3ff949d93194993a31c7f3772e9543b151c0f98e
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-27 17:21:09 -0600

    :heavy_plus_sign: ADD: Creando subcategorías

--- 11__react-router__navegacion-en-una-spa/src/pages/Categoria.jsx
+++ 11__react-router__navegacion-en-una-spa/src/pages/Categoria.jsx
@@ -3,10 +3,18 @@ import "../assets/css/blog.css";
 import { buscar } from '../api/api';
 import ListCategories from '../components/ListCategories';
 import ListPosts from '../components/ListPosts';
-import { useParams, Routes, Route } from 'react-router-dom';
+import { useParams, Routes, Route, Link, useResolvedPath } from 'react-router-dom';
 
 function Categoria() {
-    const { id } = useParams()
+    const [subcategorias, setSubcategorias] = useState([]);
+    const { id } = useParams();
+    const url = useResolvedPath("").pathname;
+
+    useEffect(() => {
+        buscar(`/categorias?id=${id}`, (response) => {
+            setSubcategorias(response[0].subcategorias);
+        });
+    }, [id]);
 
     return (
         <>
@@ -14,6 +22,17 @@ function Categoria() {
                 <h2 className='title-page'>Pet Noticias</h2>
             </div>
             <ListCategories />
+            <ul className='category-list container flex'>
+                {
+                    subcategorias.map(subcategoria => (
+                        <li className={`category-list__category category-list__category--${id}`} key={subcategoria}>
+                            <Link to={`${url}/${subcategoria}`}>
+                                {subcategoria}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
             <Routes>
                 <Route path='/' element={<ListPosts url={`/posts?categoria=${id}`} />} />
             </Routes>
