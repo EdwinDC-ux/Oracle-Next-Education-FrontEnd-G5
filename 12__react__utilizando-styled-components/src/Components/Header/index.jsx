86cc7d05a21c99945daa5f9df9896eb81b269d4d
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-08-30 10:35:26 -0600

    :heavy_plus_sign: ADD: Props

--- 12__react__utilizando-styled-components/src/Components/Header/index.jsx
+++ 12__react__utilizando-styled-components/src/Components/Header/index.jsx
@@ -17,17 +17,28 @@ const Logo = styled.img`
   width: 50px;
 `;
 
+const BtnHeader = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+  color: ${ ({primary}) => primary ? colorPrimario : 'white' };
+  background-color: ${ (props) => props.primary ? 'white' : 'transparent' }
+`;
+
 const Header = () => {
   return (
     <StyledHeader >
       <Logo src={logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <BtnHeader primary href="https://google.com">
           Ayuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </BtnHeader>
+        <BtnHeader href="https://google.com">
           Salir
-        </a>
+        </BtnHeader>
       </div>
     </StyledHeader>
   );
