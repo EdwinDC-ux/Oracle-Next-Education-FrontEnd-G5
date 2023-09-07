1dc831aecfa3f8bb087cce77492ad45a3a111ace
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-09-07 10:26:29 -0600

    :heavy_plus_sign: ADD: Layout

--- 13__react__function-components/src/App.js
+++ 13__react__function-components/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import FormSignUp from './components/FormSignUp';
+import Container from '@mui/material/Container';
 
 function App() {
   return (
-    <>
+    <Container component='section' maxWidth="sm" >
       <h1>Formulario Registro</h1>
       <FormSignUp />
-    </>
+    </Container>
   );
 }
 
