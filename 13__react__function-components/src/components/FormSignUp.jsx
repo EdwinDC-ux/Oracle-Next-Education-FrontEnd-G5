fcc24be0b3969feab1d28cceaeee98a25f7a194e
Edwin Díaz Caballero <edwin9602@gmail.com>
2023-09-07 10:44:22 -0600

    :heavy_plus_sign: ADD: Typography y Switch

--- 13__react__function-components/src/components/FormSignUp.jsx
+++ 13__react__function-components/src/components/FormSignUp.jsx
@@ -1,5 +1,8 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 function FormSignUp () {
     return <form>
@@ -24,11 +27,25 @@ function FormSignUp () {
             fullWidth
             margin='normal'
         />
+        <FormGroup>
+            <FormControlLabel
+                control={
+                    <Switch
+                        defaultChecked
+                    />
+                }
+                label='Promociones'
+            />
+            <FormControlLabel
+                control={
+                    <Switch
+                        defaultChecked
+                    />
+                }
+                label='Novedades'
+            />
+        </FormGroup>
         
-        <label>Promociones</label>
-        <input type="checkbox" />
-        <label>Novedades</label>
-        <input type="checkbox" />
         <Button variant="contained">Registrarse</Button>
     </form>
 }
